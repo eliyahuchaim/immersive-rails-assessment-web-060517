8e9dbd1faf7bc1b096c7c604f3ae7877dc6a26cb
github username <github email address>
2017-07-10 11:31:06 -0400

    are you getting this?

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,3 +1,4 @@
 class Episode < ApplicationRecord
+  has_one :appearance
   has_many :guests, through: :appearances
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,4 @@
 class Guest < ApplicationRecord
+  has_one :appearance
   has_many :episodes, through: :appearances
 end
